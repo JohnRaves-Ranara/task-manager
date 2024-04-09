e757de034d736305fe38e929507567c3491868a3
JohnRaves-Ranara <jrRanara@mcm.edu.ph>
2024-04-09 21:51:12 +0800

    i forgot what i changed here but its minor

--- src/components/TaskCard.tsx
+++ src/components/TaskCard.tsx
@@ -1,37 +1,45 @@
 import {
-    Card,
-    CardContent,
-    CardDescription,
-    CardFooter,
-    CardHeader,
-    CardTitle,
-  } from "@/components/ui/card";
-  import { Checkbox } from "@/components/ui/checkbox"
-
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Checkbox } from "@/components/ui/checkbox";
 
 type TaskCardProps = {
-    index: number;
-    title: string;
-    description: string;
-    isChecked: boolean;
-    toggleCheckBox: Function;
-    groupType: string;
-}
+  index: number;
+  title: string;
+  description: string;
+  isChecked: boolean;
+  toggleCheckBox: Function;
+  groupType: string;
+};
 
-const TaskCard = ({index, title, description, isChecked, toggleCheckBox, groupType} : TaskCardProps) => {
+const TaskCard = ({
+  index,
+  title,
+  description,
+  isChecked,
+  toggleCheckBox,
+  groupType,
+}: TaskCardProps) => {
   return (
     <Card className="max-w-[330px] bg-red-200">
       <CardHeader className="flex flex-row items-start gap-4">
-        <Checkbox checked={isChecked} onClick={() => toggleCheckBox(index, groupType)} className="size-6 mt-2"></Checkbox>
+        <Checkbox
+          checked={isChecked}
+          onClick={() => toggleCheckBox(index, groupType)}
+          className="size-6 mt-2"
+        ></Checkbox>
         <div className="space-y-4">
           <CardTitle className="text-lg">{title}</CardTitle>
-          <CardDescription>
-            {description}
-          </CardDescription>
+          <CardDescription>{description}</CardDescription>
         </div>
       </CardHeader>
     </Card>
   );
 };
 
-export default TaskCard
+export default TaskCard;
