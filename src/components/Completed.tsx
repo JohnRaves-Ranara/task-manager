c00371f799128b6fe35209b0108878a7e2acdff0
JohnRaves-Ranara <jrRanara@mcm.edu.ph>
2024-04-13 15:49:16 +0800

    chore: changed empty array checker to completedtasks

--- src/components/Completed.tsx
+++ src/components/Completed.tsx
@@ -11,7 +11,7 @@ const Completed = () => {
     <div className="space-y-4 w-full">
       <h1>Completed:</h1>
       <div className="flex gap-3 p-4 bg-green-200 min-h-[100px] rounded-lg flex-wrap">
-        {tasks.length === 0 ? (
+        {completedTasks.length === 0 ? (
           <div className="flex justify-center items-center w-full">No completed tasks yet.</div>
         ) : (
           completedTasks.map((completedTask) => (
